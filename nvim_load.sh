3cc732bb02c1a302a0eedb37c1a8552f3f611fa0
MrCont <contini.aron99@gmail.com>
2021-02-25 20:36:30 +0100

    latest

--- nvim_load.sh
+++ nvim_load.sh
@@ -13,5 +13,7 @@ echo "installing requirements"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+sudo apt install nodejs
+
 echo "nvim config successfully setup" 
 echo "now open neovim and run :PlugInstall to install the plugins"
