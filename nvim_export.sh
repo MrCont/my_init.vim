cb1fd7d8e14ff5dc6837eaf2d777dd4e80205820
MrCont <contini.aron99@gmail.com>
2021-02-26 15:14:11 +0100

    latest

--- nvim_export.sh
+++ nvim_export.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+echo "deleting if present current nvim folder in repo"
+sudo rm -r nvim
 echo "exporting neovim config"
+
 cp ~/.config/nvim nvim -r
-sudo rm nvim/autoload -r
+sudo rm -r nvim/autoload
 echo "neovim succesfully exported"
--- nvim_load.sh
+++ nvim_load.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo "Initializing..."
+echo "removing if present nvim folder in ~/.config"
+sudo rm -r ~/.config/nvim
 
 sudo apt install neovim
 
@@ -14,6 +16,8 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 sudo apt install nodejs
+sudo apt-get install npm
+sudo npm install -g neovim
 
 echo "nvim config successfully setup" 
 echo "now open neovim and run :PlugInstall to install the plugins"
