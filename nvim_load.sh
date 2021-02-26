b176337ba28e702e2e2139b2b692c9a446883115
MrCont <contini.aron99@gmail.com>
2021-02-26 21:01:13 +0100

    changed node installation

--- nvim_load.sh
+++ nvim_load.sh
@@ -16,7 +16,7 @@ echo "installing requirements"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sudo apt install nodejs
+sudo apt-get install nodejs
 sudo apt-get install npm
 sudo npm install -g neovim
 
